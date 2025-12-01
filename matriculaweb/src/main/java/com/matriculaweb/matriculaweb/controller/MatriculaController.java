ad87aebd2f002959a7c0f342c91556ce8f5adbff
Pierosebas8 <pierosebas8@hotmail.com>
2025-11-30 22:22:49 -0500

    Version Final 2.0

--- matriculaweb/src/main/java/com/matriculaweb/matriculaweb/controller/MatriculaController.java
+++ matriculaweb/src/main/java/com/matriculaweb/matriculaweb/controller/MatriculaController.java
@@ -7,16 +7,19 @@ import jakarta.servlet.http.HttpServletResponse;
 import com.matriculaweb.matriculaweb.services.MatriculaService;
 import com.matriculaweb.matriculaweb.repository.MatriculaRepository;
 import com.itextpdf.text.BaseColor;
+import com.itextpdf.text.Chunk;
 import com.itextpdf.text.Document;
 import com.itextpdf.text.Element;
 import com.itextpdf.text.Font;
 import com.itextpdf.text.FontFactory;
+import com.itextpdf.text.Image;
 import com.itextpdf.text.PageSize;
 import com.itextpdf.text.Paragraph;
 import com.itextpdf.text.Phrase;
 import com.itextpdf.text.pdf.PdfPCell;
 import com.itextpdf.text.pdf.PdfPTable;
 import com.itextpdf.text.pdf.PdfWriter;
+import com.itextpdf.text.pdf.draw.LineSeparator;
 import com.matriculaweb.matriculaweb.repository.AlumnoRepository;
 
 import org.springframework.stereotype.Controller;
@@ -130,40 +133,74 @@ public class MatriculaController {
         response.setContentType("application/pdf");
         response.setHeader("Content-Disposition", "attachment; filename=horario.pdf");
 
-        Document document = new Document(PageSize.A4);
-        PdfWriter.getInstance(document, response.getOutputStream());
-
+        Document document = new Document(PageSize.A4, 40, 40, 40, 40);
+        PdfWriter writer = PdfWriter.getInstance(document, response.getOutputStream());
         document.open();
 
-        Font titleFont = FontFactory.getFont(FontFactory.HELVETICA_BOLD, 18, BaseColor.BLACK);
+        try {
+            Image logo = Image.getInstance("matriculaweb/src/main/resources/static/assets/img/prescot.png");
+            logo.scaleToFit(80, 80);
+            logo.setAlignment(Image.ALIGN_LEFT);
+            document.add(logo);
+        } catch (Exception e) {
+            System.out.println("No se pudo cargar el logo: " + e.getMessage());
+        }
+
+        Font titleFont = FontFactory.getFont(FontFactory.HELVETICA_BOLD, 20, BaseColor.BLACK);
+
         Paragraph title = new Paragraph("Horario del Alumno", titleFont);
         title.setAlignment(Element.ALIGN_CENTER);
-        title.setSpacingAfter(20);
+        title.setSpacingBefore(10);
+        title.setSpacingAfter(15);
         document.add(title);
+
+        LineSeparator separator = new LineSeparator();
+        separator.setOffset(-2);
+        separator.setLineColor(BaseColor.GRAY);
+        document.add(new Chunk(separator));
+
+        document.add(Chunk.NEWLINE);
 
         PdfPTable table = new PdfPTable(4);
         table.setWidthPercentage(100);
-        table.setSpacingBefore(10);
-        table.setSpacingAfter(10);
+        table.setSpacingBefore(15);
+        table.setWidths(new float[] { 3, 3, 3, 2 });
 
+        Font headerFont = new Font(Font.FontFamily.HELVETICA, 12, Font.BOLD, BaseColor.WHITE);
+        BaseColor headerColor = new BaseColor(52, 73, 94);
         Stream.of("Curso", "Profesor", "Horario", "Horas Semanales")
                 .forEach(col -> {
-                    PdfPCell header = new PdfPCell(new Phrase(col));
-                    header.setBackgroundColor(BaseColor.LIGHT_GRAY);
+                    PdfPCell header = new PdfPCell(new Phrase(col, headerFont));
+                    header.setBackgroundColor(headerColor);
                     header.setHorizontalAlignment(Element.ALIGN_CENTER);
-                    header.setPadding(8);
+                    header.setPadding(10);
+                    header.setBorderColor(BaseColor.WHITE);
                     table.addCell(header);
                 });
 
+        Font bodyFont = new Font(Font.FontFamily.HELVETICA, 11);
+
         for (var m : matriculas) {
-            table.addCell(m.getSeccion().getCurso().getNombre());
-            table.addCell(m.getSeccion().getProfesor().getNombre() + " "
-                    + m.getSeccion().getProfesor().getApellido());
-            table.addCell(m.getSeccion().getHorario());
-            table.addCell(m.getSeccion().getCurso().getHorasSemanales() + " hrs");
+            table.addCell(new PdfPCell(new Phrase(m.getSeccion().getCurso().getNombre(), bodyFont)));
+            table.addCell(new PdfPCell(new Phrase(
+                    m.getSeccion().getProfesor().getNombre() + " " + m.getSeccion().getProfesor().getApellido(),
+                    bodyFont)));
+            table.addCell(new PdfPCell(new Phrase(m.getSeccion().getHorario(), bodyFont)));
+            table.addCell(new PdfPCell(new Phrase(
+                    m.getSeccion().getCurso().getHorasSemanales() + " hrs", bodyFont)));
         }
 
         document.add(table);
+
+
+        Paragraph footer = new Paragraph("Generado por el Sistema de Matrícula",
+                new Font(Font.FontFamily.HELVETICA, 10, Font.ITALIC, BaseColor.GRAY));
+
+        footer.setAlignment(Element.ALIGN_CENTER);
+        footer.setSpacingBefore(20);
+
+        document.add(footer);
+
         document.close();
     }
 
